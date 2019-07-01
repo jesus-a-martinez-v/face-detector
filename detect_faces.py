9bcbe4ed5c85efc6151677e960ed10370db11bf0
Jesús Martínez <jesus.a.martinez.v@gmail.com>
2019-07-01 16:35:38 -0500

    Optimizes imports and adds docs.

--- detect_faces.py
+++ detect_faces.py
@@ -1,13 +1,15 @@
 from __future__ import print_function
+
 import argparse
+
 import cv2
 
 from facedetector import FaceDetector
 
-argparser = argparse.ArgumentParser()
-argparser.add_argument('-f', '--face', required=True, help='Path to where the face cascade resides.')
-argparser.add_argument('-i', '--image', required=True, help='Path to where the image file resides.')
-arguments = vars(argparser.parse_args())
+argument_parser = argparse.ArgumentParser()
+argument_parser.add_argument('-f', '--face', required=True, help='Path to where the face cascade resides.')
+argument_parser.add_argument('-i', '--image', required=True, help='Path to where the image file resides.')
+arguments = vars(argument_parser.parse_args())
 
 image = cv2.imread(arguments['image'])
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -17,9 +19,9 @@ face_rectangles = face_detector.detect(gray, scale_factor=1.2, min_neighbors=5, 
 print('I found {} faces.'.format(len(face_rectangles)))
 
 green = (0, 255, 0)
-for (x, y, w, h) in face_rectangles:
-    cv2.rectangle(image, (x, y), (x + w, y + h), green, 2)
-
+thickness = 2
+for (x, y, width, height) in face_rectangles:
+    cv2.rectangle(image, (x, y), (x + width, y + height), green, thickness)
 
 cv2.imshow('Faces', image)
 cv2.waitKey(0)
--- facedetector.py
+++ facedetector.py
@@ -3,9 +3,26 @@ import cv2
 
 class FaceDetector(object):
     def __init__(self, face_cascade_path):
+        """
+        Class constructor.
+        :param face_cascade_path: Path to the XML file with the parameters used by OpenCV's underlying implementation
+        of Haar Cascades.
+        """
         self.face_cascade = cv2.CascadeClassifier(face_cascade_path)
 
     def detect(self, image, scale_factor=1.1, min_neighbors=5, min_size=(30, 3)):
+        """
+        Performs a detection on an input image. The results will vary depending on the input parameters configuration.
+        :param image: Image to detect a face.
+        :param scale_factor: How much the image will be reduced in each scale. This value helps create a scale pyramid
+        in order to detect faces of different sizes (for instance, a face closer to the camera lens will occupy more
+        space in the image than the face of a person in the background). Therefore, for example, a value 1.05 means
+        we’ll reduce the image size by 5% on each level of the pyramid
+        :param min_neighbors: Minimum of neighbors used to consider a rectangle or region a face.
+        :param min_size: A tuple of width and height (expressed in pixels) that establishes the minimum size a window
+        must have to be taken into consideration. Windows that fail this size test will be discarded.
+        :return: A list of rectangles of possible detections.
+        """
         rectangles = self.face_cascade.detectMultiScale(image, scaleFactor=scale_factor, minNeighbors=min_neighbors,
                                                         minSize=min_size, flags=cv2.CASCADE_SCALE_IMAGE)
 
